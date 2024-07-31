f7f8187d45e6dd68f2999acfcb7f314405b1c204
Hrithik Sagar <43140053+hrithiksagar@users.noreply.github.com>
2024-07-31 17:16:27 +0530

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,4 +1,10 @@
 # accessing apple metal GPU instead of CUDA
 import torch
-device = torch.device("mps" if torch.cuda.is_available() else "cpu")
-model.to(device)
+
+if torch.backends.mps.is_available():
+    print("MPS backend is available.")
+else:
+    print("MPS backend is not available.")
+    
+device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+print(device, "- Is now assigned to this device")
